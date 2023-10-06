91e39b70d1019d8b6fe45dda038c3fed2a05c1cc
subrrahmanyem <subbuthoto91@gmail.com>
2023-10-06 20:50:42 +0530

    commit

--- backend/src/controllers/userAuthentication.js
+++ backend/src/controllers/userAuthentication.js
@@ -2,8 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const Registeruser = require('../models/userDetails');
-const middleware = require('../middleware/authMiddleware');
-
+const middleware = require('../middleware/authMiddleware.js');
 
 const router = express.Router();
 
